537169e926f5aacec606bad965b046e54651f37a
Alex Red <red@mail.com>
2021-05-27 01:25:24 +0100

    simple page

--- less_1.py
+++ less_1.py
@@ -14,36 +14,6 @@ settings.configure(
     SECRET_KEY="sdasdas"
 )
 
-template = """
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>{title}</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.2/css/bulma.min.css">
-  </head>
-  <body>
-  <section class="section">
-    <div class="container">
-         <div class="box">
-          {quote}
-        </div>
-         <div class="box">
-          {quote_two}
-        </div>
-    </div>
-  </section>
-  </body>
-</html>
-"""
-text = ''.join(this.d.get(c, c) for c in this.s)
-
-title, _, *quotes = text.splitlines()
-
-
-def hey(_):
-    return HttpResponse(template.format(title=title, quote=choice(quotes), quote_two=choice(quotes)))
 
 
 def universe(_):
@@ -51,8 +21,8 @@ def universe(_):
 
 
 urlpatterns = [
-    path('', hey),
-    path('retro', universe)
+
+    path('/', universe)
 ]
 
 execute_from_command_line(sys.argv)
